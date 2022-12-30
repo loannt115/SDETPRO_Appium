a8eb0cf592d18246403cf1a5490c30014ac0aa01
Loan Nguyen <muatrenphobayxa115@gmail.com>
2022-12-30 16:52:15 +0700

    lesson 16

---
+++ src/test/java/api_learning/SwipeVertically.java
@@ -0,0 +1,80 @@
+package api_learning;
+
+import driver.DriverFactory;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import platform.Platform;
+
+import java.time.Duration;
+import java.util.List;
+
+public class SwipeVertically {
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platform.ANDROID);
+
+        try {
+            MobileElement navFormScreenBtnElem = appiumDriver.findElement(MobileBy.AccessibilityId("Forms"));
+            navFormScreenBtnElem.click();
+
+            //Wait until user is on Form screen
+            WebDriverWait wait = new WebDriverWait(appiumDriver, 5L);
+            wait.until(ExpectedConditions
+                    .visibilityOfElementLocated(MobileBy.AndroidUIAutomator("new UiSelector().textContains(\"Form Components\")")));
+
+            //Get mobile window size
+            Dimension windowSize = appiumDriver.manage().window().getSize();
+            int screenHeight = windowSize.getHeight();
+            int screenWidth = windowSize.getWidth();
+
+            //Calculate touch points
+            int xStartPoint = 50 * screenWidth / 100;
+            int xEndPoint = 50 * screenWidth / 100;
+
+            int yStartPoint = 50 * screenHeight / 100;
+            int yEndPoint = 10 * screenHeight / 100;
+
+            //Convert coordinates -> PointOption
+            PointOption startPoint = new PointOption<>().withCoordinates(xStartPoint, yStartPoint);
+            PointOption endPoint = new PointOption<>().withCoordinates(xEndPoint, yEndPoint);
+
+            //Using TouchAction to swipe
+            TouchAction touchAction = new TouchAction(appiumDriver);
+
+            //Swipe up
+            touchAction
+                    .press(startPoint)
+                    .waitAction(new WaitOptions().withDuration(Duration.ofMillis(500)))
+                    .moveTo(endPoint)
+                    .release()
+                    .perform();
+
+            //Swipe down | trick: revert coordinates
+            /*.press(endPoint).waitAction(new WaitOptions().withDuration(Duration.ofMillis(500)))
+             * can be replaced to: longPress(endPoint)
+             */
+            touchAction
+                    .longPress(endPoint)
+                    .moveTo(startPoint)
+                    .release()
+                    .perform();
+
+            //Click on button Active
+//            MobileElement activeBtnElem = appiumDriver.findElement(MobileBy.AccessibilityId("button-Active"));
+//            activeBtnElem.click();
+
+            //debug only
+            Thread.sleep(3000);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        appiumDriver.quit();
+    }
+}
---
+++ src/test/java/api_learning/XPathLearning.java
@@ -0,0 +1,41 @@
+package api_learning;
+
+import driver.DriverFactory;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import platform.Platform;
+
+import java.util.List;
+
+public class XPathLearning {
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platform.ANDROID);
+
+        try {
+            MobileElement navLoginScreenBtnElem = appiumDriver.findElement(MobileBy.AccessibilityId("Login"));
+            navLoginScreenBtnElem.click();
+
+            //Find all matching elements
+            List<MobileElement> creFieldsElem = appiumDriver.findElements(MobileBy.xpath("//android.widget.EditText"));
+            final int USERNAME_INDEX = 0;
+            final int PASSWORD_INDEX = 1;
+            creFieldsElem.get(USERNAME_INDEX).sendKeys("teo@sth.com");
+            creFieldsElem.get(PASSWORD_INDEX).sendKeys("12345678");
+
+            //Find login info text by UiSelector
+            MobileElement loginInstructionElem =
+                    appiumDriver.findElement(MobileBy.AndroidUIAutomator("new UiSelector().className(\"android.widget.TextView\").textContains(\"When the device\")"));
+            System.out.println(loginInstructionElem.getText());
+
+            //debug only
+            Thread.sleep(3000);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        appiumDriver.quit();
+    }
+}
