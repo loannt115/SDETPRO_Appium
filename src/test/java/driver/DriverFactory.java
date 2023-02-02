6d06c34bf84b77c9ef7997aa23273c132486d0fe
Loan Nguyen <muatrenphobayxa115@gmail.com>
2023-02-02 16:30:50 +0700

    lesson 22; TestNG: parameter, parallel; isolate DriverFactory instances

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -11,6 +11,7 @@ import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class DriverFactory implements MobileCapabilityTypeEx {
+    private AppiumDriver<MobileElement> appiumDriver;
     public static AppiumDriver<MobileElement> getDriver(Platform platform) {
         AppiumDriver<MobileElement> appiumDriver = null;
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
@@ -43,5 +44,47 @@ public class DriverFactory implements MobileCapabilityTypeEx {
             appiumDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         }
         return appiumDriver;
+    }
+    public AppiumDriver<MobileElement> getDriver(Platform platform, String udid, String systemPort) {
+        if (appiumDriver == null){
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setCapability(PLATFORM_NAME, "Android");
+            desiredCapabilities.setCapability(AUTOMATION_NAME, "uiautomator2");
+            desiredCapabilities.setCapability(UDID, udid);
+            desiredCapabilities.setCapability(APP_PACKAGE, "com.wdiodemoapp");
+            desiredCapabilities.setCapability(APP_ACTIVITY, ".MainActivity t12");
+            desiredCapabilities.setCapability(SYSTEM_PORT, systemPort);
+            URL appiumServer = null;
+            try {
+                appiumServer = new URL("http://localhost:4723/wd/hub");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (appiumServer == null)
+                throw new RuntimeException("Can't construct the appium server url @http://localhost:4723/wd/hub");
+
+            switch (platform) {
+                case ANDROID:
+                    appiumDriver = new AndroidDriver<>(appiumServer, desiredCapabilities);
+                    break;
+                case IOS:
+                    appiumDriver = new IOSDriver<>(appiumServer, desiredCapabilities);
+                    break;
+            }
+
+            if (appiumDriver != null) {
+                //Implicit Wait | Interval time 500ms
+                appiumDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            }
+        }
+        return appiumDriver;
+    }
+
+    public void quitAppiumDriver(){
+        if (appiumDriver != null){
+            appiumDriver.quit();
+            appiumDriver = null;
+        }
     }
 }
--- src/test/java/tests/parallel/LoginDevice01.java
+++ src/test/java/tests/parallel/LoginDevice01.java
@@ -1,4 +1,4 @@
-package tests.authen;
+package tests.parallel;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -7,9 +7,10 @@ import test_data.models.LoginCred;
 import test_flows.authentication.LoginFlow;
 import tests.BaseTest;
 
-public class LoginWithBaseTest extends BaseTest {
+public class LoginDevice01 extends BaseTest {
     @Test(dataProvider = "loginCredData")
     public void testLogin(LoginCred loginCred) {
+        System.out.println("Session ID: " + getDriver().getSessionId());
         LoginFlow loginFlow = new LoginFlow(getDriver(), loginCred.getEmail(), loginCred.getPassword());
         loginFlow.goToLoginScreen();
         loginFlow.login();
